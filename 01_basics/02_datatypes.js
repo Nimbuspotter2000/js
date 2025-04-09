b5d8fe89ba0471d35c808f0e72ba94a32b763851
Nimbuspotter2000 <67220765+Nimbuspotter2000@users.noreply.github.com>
2025-04-09 08:37:33 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+//alert("3+3") //we are using nodejs not browser
+
+let name = "Anand"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value not asssigned yet
+//symbol => unique
+
+//object
+
+console.log(typeof "Anand")
+console.log(typeof age)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
