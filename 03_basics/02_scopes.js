d48a9929ac4267ec842d5ab250d6fe487efef7a9
Nimbuspotter2000 <67220765+Nimbuspotter2000@users.noreply.github.com>
2025-04-13 12:10:33 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,17 +5,53 @@ if(true){
     const b = 20 
     var c = 30
     d = 40
-    console.log("inner: ", a);
+    //console.log("inner: ", a);
 }
 
 
-console.log(a);//satisfies scope 
+//console.log(a);//satisfies scope 
 //console.log(b);//satisfies scope
-console.log(c); //doesnot satisfy scope
-console.log(d); //doesnot satisfy scope
+//console.log(c); //doesnot satisfy scope
+//console.log(d); //doesnot satisfy scope
 
 /*
 note - global scope of the browser 
 and global scope of node(code environment) 
 are different
 */
+
+function one(){
+    const userName = "Anand"
+
+    function two(){
+        const website = "youtube"
+        console.log(userName)
+    }
+    //console.log(wesite);
+    //two()
+}
+//one()
+
+//************  interesting *********//
+console.log(addOne(5)) //works fine even when 
+// we have called the function before declaring it
+function addOne(num){
+    //this is function
+    return num + 1
+}
+
+console.log(addTwo(5)) //wont work because
+//of the way in which we have declared the function
+//concept of hoisting
+const addTwo = function(num){
+    //this one called function expression
+    return num + 2
+}
+
+/* JavaScript hoists function declarations to the top of their scope, meaning
+The whole function is available before it's even written.
+So, addOne() is ready to use before the interpreter reaches its definition.
+
+const addTwo = function(num) {...} is NOT hoisted like a regular function.
+ */
+
