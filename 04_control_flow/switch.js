f671ad1dd30b5a2a45a8ad8a89b9042099ca28ea
Nimbuspotter2000 <67220765+Nimbuspotter2000@users.noreply.github.com>
2025-04-14 10:16:18 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,39 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+// const month = "march"
+
+// switch (month) {
+//     case "jan":
+//         console.log("january");
+//         break;
+//     case "feb":
+//         console.log("feb");
+//         break;
+//     case "march":
+//         console.log("march");
+//         break;
+//     case "april":
+//         console.log("april");
+//         break;
+
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+/*
+if case march(jo ki match wala hai) me break nahi 
+hota to uske baad k saare case
+ko execute karta except default
+that is output me march and april dono aata 
+if matched case me break statement hata de to
+except default
+*/
